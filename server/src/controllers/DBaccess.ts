6d3cb7808c28f8097c44b430aa9fb9dd3bb70760
jepuli124 <konsta.jalkanen.kj@gmail.com>
2025-12-16 16:23:24 +0200

    some conf to mine
    can't be run yet

---
+++ server/src/controllers/DBaccess.ts
@@ -0,0 +1,93 @@
+
+export const markersByMapId = async (req: any, res: any) => { // returns all markers by mapID in a list
+    try {
+        const markers = await Marker.find({mapThisBelongsTo: req.params["id"]})
+        if(!markers){
+            return res.status(404).json({msg: "Markers not found"})
+        }
+        return res.status(200).json(markers) 
+
+    } catch (error: any) {
+        console.log(`Error while fetching tags: ${error}`)
+        return res.status(500).json({message: "Internal server error"})
+    }
+    }
+
+export const uploadMarker = async (req: any, res: any) => { //upload a marker
+    try {
+        let campain: string = ""
+        let tags: string[] = []
+        if(req.body.linkToAnotherMap){
+            const map: ILocationMap | null = await LocationMap.findById(req.body.linkToAnotherMap)
+            if(map){
+                map.tags.forEach((tag) => {
+                    tags.push(tag.name)
+                })
+                campain = map.campain
+            }
+        }
+
+        let newMarker: IMarker = new Marker ({
+            x: req.body.x,
+            y: req.body.y,
+            color: req.body.color,
+            symbol: req.body.symbol,
+            mapThisBelongsTo: req.body.mapThisBelongsTo,
+            tags: tags,
+            campain: campain,
+            linkToAnotherMap: req.body.linkToAnotherMap
+        })
+        await newMarker.save()
+        return res.status(201).json({msg: `successful operation in upload map marker`})
+    } catch (error: any) {
+        console.log(`Error while uploading location map: ${error}`)
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const editMarker = async (req: any, res: any) => { //upload a marker
+    try {
+        if(req.body.linkToAnotherMap){
+            let campain: string = ""
+            let tags: string[] = []
+            const map: ILocationMap | null = await LocationMap.findById(req.body.linkToAnotherMap)
+            if(map){
+                map.tags.forEach((tag) => {
+                    tags.push(tag.name)
+                })
+                campain = map.campain
+            }
+            const marker = await Marker.findByIdAndUpdate(req.params.id, {
+            color: req.body.color,
+            symbol: req.body.symbol,
+            tags: tags,
+            campain: campain,
+            mapThisBelongsTo: req.body.mapThisBelongsTo,
+            linkToAnotherMap: req.body.linkToAnotherMap
+            })
+        } else {
+            const marker = await Marker.findByIdAndUpdate(req.params.id, {
+            color: req.body.color,
+            symbol: req.body.symbol,
+            mapThisBelongsTo: req.body.mapThisBelongsTo,
+            linkToAnotherMap: req.body.linkToAnotherMap
+            })
+        }
+        
+        return res.status(201).json({msg: `successful operation in updating marker`})
+    } catch (error: any) {
+        console.log(`Error while uploading location map: ${error}`)
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const deleteMarker = async (req: any, res: any) => { //delete a  marker
+    try {
+
+        await Marker.deleteOne({_id: req.params.id})
+        return res.status(201).json({msg: `successfully deleted marker`})
+    } catch (error: any) {
+        console.log(`Error while uploading location map: ${error}`)
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,15 +1,20 @@
 import mongoose, {Date, Document, Schema} from "mongoose";
 
 interface IUser extends Document{
+    userID?: string
     username: string
-    password: string
-    admin: boolean
+    passwordHash: string
+    isAdmin: boolean
+    email: string
+    createdAt: Date
 }
 
 let users: Schema = new Schema ({
     username: {type: String, require: true},
-    password: {type: String, require: true},
-    admin: {type: Boolean, require: true}
+    passwordHash: {type: String, require: true},
+    isAdmin: {type: Boolean, require: true},
+    email: {type: String, require: true},
+    createdAt: {type: Date, require: true}
 })
 
 const User: mongoose.Model<IUser> = mongoose.model<IUser>("users", users)

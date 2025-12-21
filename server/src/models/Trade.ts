a37bf0fa6690df8eb7505612cc685c7f5e29a01f
LETomppa <vilpponent@gmail.com>
2025-12-21 18:20:17 +0200

    Changed model to have correct name

--- server/src/models/Trade.ts
+++ server/src/models/Trade.ts
@@ -4,14 +4,14 @@ interface ITrade extends Document{
     tradeID: string
     senderID: string
     receiverID: string
-    itemId: string
+    itemID: string
 }
 
 let trades: Schema = new Schema ({
     tradeID: {type: String, require: true},
     senderID: {type: String, require: true},
     receiverID: {type: String, require: true},
-    itemId: {type: String, require: true}
+    itemID: {type: String, require: true}
 })
 
 const Trade: mongoose.Model<ITrade> = mongoose.model<ITrade>("Trade", trades)
